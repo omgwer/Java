6f8b58b4e3727ef0c609dcc41249ffae43295aa0
omgwer <63747048+omgwer@users.noreply.github.com>
2021-12-05 11:58:06 +0300

    upd

---
+++ src/Java/Lesson11/Lesson11.java
@@ -0,0 +1,12 @@
+package Lesson11;
+
+public class Lesson11 {
+    public static void main(String[] args) {
+        if (true) {
+            int a = 0;
+        } else if(true) {
+
+        }
+
+    }
+}
